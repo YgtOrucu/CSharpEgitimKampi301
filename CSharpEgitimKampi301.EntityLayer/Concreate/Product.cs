1b76e4a1951a3a9a72ea2acd006c9899a228cabb
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2025-10-10 21:05:00 +0300

    Add multi-layered C# project with Entity Framework

---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
